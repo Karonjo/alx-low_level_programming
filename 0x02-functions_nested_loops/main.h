2bc630d4539869a263355b9de67bc5e9e25ae825
Karonjo <felixkaronjo@gmail.com>
2023-07-19 08:46:44 -0700

    added

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifindef main_h
+#ifndef main_h
 #define main_h
 
 int _putchar(char);
@@ -13,6 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n):
+void print_times_table(int n);
 
 #endif
